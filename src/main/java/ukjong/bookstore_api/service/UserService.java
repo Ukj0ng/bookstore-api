e119b731657dcf86ce0945168f79390bbf813534
Ukj0ng <xb253q@gmail.com>
2025-07-01 19:41:40 +0900

    fix: transactional 라이브러리 변경

--- src/main/java/ukjong/bookstore_api/service/UserService.java
+++ src/main/java/ukjong/bookstore_api/service/UserService.java
@@ -1,6 +1,6 @@
 package ukjong.bookstore_api.service;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
