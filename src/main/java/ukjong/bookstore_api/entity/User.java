60e6185977646071aa4b8ba1c13b6fb7afb6abbe
Ukj0ng <xb253q@gmail.com>
2025-07-01 17:22:57 +0900

    feat: User builder 패턴 적용

--- src/main/java/ukjong/bookstore_api/entity/User.java
+++ src/main/java/ukjong/bookstore_api/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Data
 @Entity
+@Builder
 @Table(name = "users")
 public class User implements UserDetails {
 
