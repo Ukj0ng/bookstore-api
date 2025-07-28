be579335122a78b04ff6bfd747219d0440fa242a
Ukj0ng <xb253q@gmail.com>
2025-07-28 21:50:08 +0900

    배포 스터디

--- src/main/java/ukjong/bookstore_api/controller/HomeController.java
+++ src/main/java/ukjong/bookstore_api/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public void test() {
-        System.out.println("asdf");
+    public String test() {
+        return "success";
     }
 }
